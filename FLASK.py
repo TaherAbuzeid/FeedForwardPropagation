0b6d2ec7c19672184232a0be1bd19ec43c466654
taherabuzeid <taherabuzied881@gmail.com>
2022-06-10 13:34:57 +0200

    add lines

--- FLASK.py
+++ FLASK.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 skills_app = Flask(__name__)
 
 
 @skills_app.route("/")
 def homepage():
-    return "Hello From Flask Framework"
+    return render_template("homepage.html")
 
 @skills_app.route("/about")
 def about():
     return "About Page From Flask Framework"
+
+if __name__ == "__main__":
+    skills_app.run(debug=True , port=9000)
